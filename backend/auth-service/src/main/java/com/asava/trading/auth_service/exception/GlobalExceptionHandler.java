e4e779a8ffcb1d1aedde75879194132ea1daa09f
sarthakdesh2104-glitch <sarthakdesh2104@gmail.com>
2026-03-17 12:59:48 +0530

    Added service layer and exception handling

---
+++ backend/auth-service/src/main/java/com/asava/trading/auth_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.asava.trading.auth_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGlobal(Exception ex) {
+        return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ backend/auth-service/src/main/java/com/asava/trading/auth_service/service/AuthService.java
@@ -0,0 +1,5 @@
+package com.asava.trading.auth_service.service;
+
+public interface AuthService {
+    String login(String username, String password);
+}
